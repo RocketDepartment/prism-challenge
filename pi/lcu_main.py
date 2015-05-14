4184c40854f9aa49b1e8c5d23e0f1d5204aff4f9
Emma <emma@rocketdept.com>
2015-05-14 00:18:05 -0700

    enables communication from arduino to RPi

--- pi/lcu_main.py
+++ pi/lcu_main.py
@@ -6,8 +6,19 @@
 # Emma Fletcher
 # 05.13.2015
 
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+arduinoInput = 20
+GPIO.setup(arduinoInput, GPIO.IN)
+
 def main():
 	print("Hello Laser world :)")
+
+	while(1):
+		val = GPIO.input(arduinoInput)
+		print(val)
 
 if __name__ == "__main__":
 	main()
